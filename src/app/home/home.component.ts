f09ff5751d89d534418f5baca97c32ab7570c9d9
Adriaris <leonarisadria@gmail.com>
2024-02-10 19:17:02 +0100

    Porfolio 10 de feb

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +7,56 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  typewriterText: string = "";
+  isBlinking: boolean = true; // Controla el estado del parpadeo del cursor
+  private texts: string[] = ["Adrià León", "Desarrollador Web", "Desarrollador Multiplataforma"];
+  private textIndex: number = 0;
+  private letterIndex: number = 0;
 
+  constructor() { }
+
+  ngOnInit(): void {
+    this.typeNextText();
+  }
+
+  private typeNextText(): void {
+    if (this.textIndex >= this.texts.length) {
+      this.textIndex = 0;
+    }
+    this.typeLetter();
+  }
+
+  private typeLetter(): void {
+    this.isBlinking = false; // Detiene el parpadeo mientras se escribe
+    const currentText = this.texts[this.textIndex];
+    this.typewriterText = currentText.slice(0, ++this.letterIndex);
+
+    if (this.letterIndex <= currentText.length) {
+      setTimeout(() => this.typeLetter(), 50);
+    } else {
+      this.isBlinking = true; // Reanuda el parpadeo una vez terminada la escritura
+      setTimeout(() => {
+        this.letterIndex = 0;
+        this.typewriterText = ""; // Limpiar el texto antes de empezar el siguiente
+        this.textIndex++;
+        this.typeNextText();
+      }, 2000);
+    }
+  }
+
+  downloadCv() {
+    // Path relativo al archivo PDF dentro de la carpeta de assets
+    const pdfUrl = './assets/cv.pdf';
+    // Nombre que deseas que tenga el archivo al ser descargado
+    const pdfName = 'CV-AdriaLeonAris';
+    // Crear un link temporal y simular un click para descargar
+    const downloadLink = document.createElement('a');
+    downloadLink.href = pdfUrl;
+    downloadLink.download = pdfName;
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+    document.body.removeChild(downloadLink);
+  }
 }
+
--- src/app/mis-proyectos/mis-proyectos.component.ts
+++ src/app/mis-proyectos/mis-proyectos.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-mis-proyectos',
@@ -7,6 +7,31 @@ import { Component } from '@angular/core';
   templateUrl: './mis-proyectos.component.html',
   styleUrl: './mis-proyectos.component.css'
 })
-export class MisProyectosComponent {
+export class MisProyectosComponent implements OnInit {
 
+  proyectos = [
+    {
+      imagen: 'assets/images/ImagenProyecto1.webp',
+      titulo: 'Playmate',
+      subtitulo: 'Proyecto transversal de segundo  - Desarrollo Web',
+      descripcion: 'Desarrollé individualmente "Playmate", una plataforma web dirigida a jugadores de videojuegos, con el fin de facilitar la conexión, comunicación y formación de nuevas amistades entre ellos. Utilicé Angular y Symfony para construir una aplicación completa con base de datos, enfrentando el desafío de trabajar solo y bajo limitaciones de tiempo. A pesar de estos retos, el proyecto fue un éxito.'
+    },
+    {
+      imagen: 'assets/images/ImagenProyecto2.png',
+      titulo: 'Petcare',
+      subtitulo: 'Proyecto transversal de segundo  - Desarrollo Multiplataforma',
+      descripcion: 'En equipo, desarrollamos "PetCare", una aplicación móvil diseñada para unir a dueños de mascotas con cuidadores fiables. Inspirados en el modelo de Wallapop, implementamos un sistema de valoraciones y filtros usando Kotlin y Java Spring.'
+    },
+    {
+      imagen: 'assets/images/ImagenProyecto3.png',
+      titulo: 'Mi porfolio',
+      subtitulo: 'Desarrollo web personal ',
+      descripcion: 'Utilizando Angular 17, he diseñado e implementado un sitio web que no solo exhibe mis proyectos sino que también refleja mi pasión y habilidades en el desarrollo web. Demostrando lo que soy capaz de hacer, y cómo puedo aportar valor a futuros proyectos y colaboraciones'
+    }
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
 }
