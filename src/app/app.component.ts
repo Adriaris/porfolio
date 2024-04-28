359b62a61ed214d0a233ade6dab7bed21d4eb050
Adriaris <leonarisadria@gmail.com>
2024-04-29 01:22:47 +0200

    porfolio final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,17 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
+import { MiValorComponent } from "./mi-valor/mi-valor.component";
+import { HomeComponent } from "./home/home.component";
+import { SobreMiComponent } from "./sobre-mi/sobre-mi.component";
+import { MisProyectosComponent } from "./mis-proyectos/mis-proyectos.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet, NavbarComponent, RouterLink],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [CommonModule, RouterOutlet, NavbarComponent, RouterLink, MiValorComponent, HomeComponent, SobreMiComponent, MisProyectosComponent]
 })
 export class AppComponent {
   title = 'porfolio-angular17';
